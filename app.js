4d5a4869285524e17c423437f259a355cb1aa043
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-25 21:27:43 -0600

    cambios

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ window.addEventListener('load', ()=> {
 
         lon= posicion.coords.longitude
         lat= posicion.coords.latitude
-
-        const url= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e89a76f7d63f4fdf3bf683f33b7d184d`
+        //ubicacion actual
+        //const url= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e89a76f7d63f4fdf3bf683f33b7d184d`
+        
+        
+        //ubicacion exacta por ciudad
+        const url= `https://api.openweathermap.org/data/2.5/weather?q=Guatemala&lang=es&units=metric&appid=e89a76f7d63f4fdf3bf683f33b7d184d`
         console.log(url);
     })
     }
