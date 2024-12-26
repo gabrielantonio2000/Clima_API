131fbaab65bb540867bd93e3301c0deca6af54c0
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-25 22:39:45 -0600

    cambios

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ window.addEventListener('load', ()=> {
     let temperaturaDescripcion= document.getElementById('temperatura-descripcion')
     //caja2
     let ubicacion= document.getElementById('ubicacion')
-    let iconoAnimado= document.getElementById=('icono-animado')
+    let iconoAnimado= document.getElementById('icono-animado')
 
     //caja3
     let vientoVelocidad= document.getElementById('viento-velocidad')
@@ -20,7 +20,7 @@ window.addEventListener('load', ()=> {
         lon= posicion.coords.longitude
         lat= posicion.coords.latitude
         //ubicacion actual
-        //const url= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e89a76f7d63f4fdf3bf683f33b7d184d`
+        //const url= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&appid=e89a76f7d63f4fdf3bf683f33b7d184d`
         
         
         //ubicacion exacta por ciudad
@@ -31,9 +31,28 @@ window.addEventListener('load', ()=> {
         fetch(url)
         .then(response => {return response.json()})
         .then(data => {
-            console.log(data)
-        })
+            //console.log(data.main.temp)
+        let temp= Math.round(data.main.temp)
+        temperaturaValor.textContent= `${temp} °C`
+        //console.log(data.weather[0].description)
+        let desc= (data.weather[0].description)
+        temperaturaDescripcion.textContent=desc.toUpperCase()
+        
+        console.log(data.name)
+        ubicacion.textContent = data.name
 
+        console.log(data.wind.speed)
+        vientoVelocidad.textContent = `${data.wind.speed} m/s`
+
+
+        //PARA TRABAJAR LOS ICONOS ESTATICOS DE LA API 
+        // console.log(data.weather[0].icon)
+        // let iconCode= data.weather[0].icon
+        // const urlIcon= `https://openweathermap.org/img/wn/${iconCode}.png`
+        // console.log(urlIcon)
+        
+    
+    })
         .catch(error =>{
             console.log(error)
         })
